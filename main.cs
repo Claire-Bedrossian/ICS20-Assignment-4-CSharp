d5071b5e236fdf9b15f051e81cf242f133ab1a9b
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-12-02 18:57:44 -0500

    fixed whitespace 2

--- main.cs
+++ main.cs
@@ -10,14 +10,15 @@ class Program
     public static void Main(string[] args)
     {
         System.Random random = new System.Random();
-        
+
         // This function accepts user input
+
         double sizeCost;
         double typeCost;
         double subtotal;
         double finalCost;
         double finalCostDiscount;
-    
+
         Console.WriteLine(" Order your coffee below. There is a twenty five percent discount for orders $ 3.50 and above...");
         Console.WriteLine("");
         Console.WriteLine("Coffee Sizes: Large $ 3.50, Medium $ 2.75, Small $ 1.50 ");
@@ -30,28 +31,29 @@ class Program
         Console.Write("Enter the cost of the type of coffee: ");
         typeCost = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("");
-      
+
         subtotal = sizeCost + typeCost;
         finalCost = subtotal + (subtotal * 0.13);
         finalCostDiscount = subtotal + (subtotal * 0.13) - (subtotal * 0.25);
         if (subtotal >= 3.50)
         {
         Console.WriteLine("Your subtotal is: " + subtotal.ToString("0.00") + ". ");
-          Console.WriteLine("");
-          Console.WriteLine("HST: 13% ");
-          Console.WriteLine("");
-          Console.WriteLine("Your final cost is: " + finalCostDiscount.ToString("0.00") + ". ");
-          } 
+        Console.WriteLine("");
+        Console.WriteLine("HST: 13% ");
+        Console.WriteLine("");
+        Console.WriteLine("Your final cost is: " + finalCostDiscount.ToString("0.00") + ". ");
+        }
     else
-       {
-          Console.WriteLine("Your subtotal is: " + subtotal.ToString("0.00") + ". ");
-          Console.WriteLine("");
-          Console.WriteLine("HST: 13%");
-          Console.WriteLine("");
-          Console.WriteLine("Discount: 25%");
-          Console.WriteLine("");
-          Console.WriteLine("Your final cost is: " + finalCost.ToString("0.00") + ". ");        }
-          Console.WriteLine("");
-          Console.WriteLine("\nDone.");
+        {
+        Console.WriteLine("Your subtotal is: " + subtotal.ToString("0.00") + ". ");
+        Console.WriteLine("");
+        Console.WriteLine("HST: 13%");
+        Console.WriteLine("");
+        Console.WriteLine("Discount: 25%");
+        Console.WriteLine("");
+        Console.WriteLine("Your final cost is: " + finalCost.ToString("0.00") + ". ");
+        }
+        Console.WriteLine("");
+        Console.WriteLine("\nDone.");
     }
 }
